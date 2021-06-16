c0f9a60835230aadfaae01b41ad82bb9a0830e0d
Belal Shehab <ma43222@gmail.com>
2021-06-16 11:32:39 +0200

    ignore output files

--- book.py
+++
@@ -1,9 +0,0 @@
-class Book:
-    def __init__(self):
-        self.ocr_output = ''
-        self.name = ''
-        self.book_url = ''
-        self.library = ''
-        self.shelve = ''
-        self.index = ''
-
